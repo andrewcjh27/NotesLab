cc27226904c7125ccc9bbeb13d88546ecf516a83
andrewcjh27 <andrewc27@berkeley.edu>
2026-01-26 09:42:07 -0800

    Initial Commit

---
+++ NotesLab/NotesLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesLabApp.swift
+//  NotesLab
+//
+//  Created by Andrew Choi on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct NotesLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
